3a173da7a3662e62fd1c879f1d0aac44fda1634b
MauMG03 <mauricio.munoz.gutierrez@correounivalle.edu.co>
2024-10-15 23:28:48 -0500

    Added interview save session

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from "firebase/firestore";
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -16,4 +17,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const firestore = getFirestore(app);
 export default app;
